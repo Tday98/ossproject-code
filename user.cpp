f0d7ed68a7fa2afe9c298fc0cad93ec80a65d634
TDay98 <tmd526@gmail.com>
2025-02-02 14:14:23 -0600

    Added more user.cpp functionality..

--- user.cpp
+++ user.cpp
@@ -8,9 +8,12 @@ int main(int argc, char** argv) {
 		printf(" I got %d arguments: \n", argc);
 
 		int i;
-		for (i =0; i < argc; i++)
-				printf("|%s| ", argv[i]);
-		printf("\nChild is now ending.\n");
+		for (i =0; i < argc; i++) {
+			printf("USER PID:%d  PPID:%d Iteration:%d before sleeping\n", getpid(), getppid(), i+1);
+			sleep(1);
+			printf("USER PID:%d  PPID:%d Iteration:%d after sleeping\n", getpid(), getppid(), i+1);
+		}
+		printf("\nUser is now ending.\n");
 
 		sleep(3);
 		return EXIT_SUCCESS;
