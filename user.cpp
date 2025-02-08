9fd15c2e3a4bbab229e50f360bc24639b7254c1f
TDay98 <tmd526@gmail.com>
2025-02-08 13:51:11 -0600

    Fixed argv parsing bug

--- user.cpp
+++ user.cpp
@@ -4,7 +4,10 @@
 #include<stdlib.h>
 int main(int argc, char** argv) {
 		int i;
-		for (i =0; i < argc; i++) {
+		int iteration = atoi(argv[1]);
+		if (argc < 2) 
+			return EXIT_FAILURE;
+		for (i =0; i < iteration; i++) {
 			printf("USER PID:%d  PPID:%d Iteration:%d before sleeping\n", getpid(), getppid(), i+1);
 			sleep(1);
 			printf("USER PID:%d  PPID:%d Iteration:%d after sleeping\n", getpid(), getppid(), i+1);
